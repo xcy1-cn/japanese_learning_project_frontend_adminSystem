2a4fca9e443a65db9c17851fb6ae84a94c0f795f
xcy <1611517164@qq.com>
2026-05-31 15:53:58 +0800

    add: add plugins to enhance developping experience

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import "@/styles/index.scss";
 
 import App from "./App.vue";
 import router from "./router";
+import "normalize.css";
 
 const app = createApp(App);
 
